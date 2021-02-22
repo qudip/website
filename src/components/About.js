874f9da93265314895e718bcd1af9394e46be2c4
qudip <quinn.dipaolo@gmail.com>
2021-02-22 14:30:21 -0500

    showing bart

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,7 @@ import '../App.css'
 
 function About() {
 
-  const fuck=<a  className="a" href="/eggone">A collection of my interests and skills. 
-                                  You can also find the first step of my 
-                                  easter egg here</a>
+  const fuck=<a  className="a" href="/eggone">A collection of my interests and skills. </a>
   const headertext = {
     fontSize: '36pt',
     textAlign: 'center',
@@ -30,10 +28,10 @@ function About() {
                subheader={fuck}
       /><br/>
         
-      <blockquote className="blockquote" style={headertext}>BARTS A BITCH</blockquote>
+      <blockquote className="blockquote" style={headertext}>Who I am</blockquote>
       <blockquote className="blockquote" style={text}>
       I have always been obsessed with computers and how we interact with them. The evolution of technology fascinates me and I hope to someday contribute to it in some capacity. During my studies, I became extremely proficient in C as well as working on full-stack apps in an agile environment. Moreover,
-my job experience allowed me to gain experience in testing and QA. As I am now independently learning I am focusing on mobile computing and game development.
+my job experience allowed me to gain experience in testing and QA. As I am now independently learning I am focusing on mobile computing, game development, neural networks, RPA, and more.
       </blockquote><br/><br />
         
       <blockquote className="blockquote" style={headertext}>Skills</blockquote>
